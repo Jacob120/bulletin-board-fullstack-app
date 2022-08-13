9554ab06ab56866d7272b88641adf7e113e95587
Jakub Kozik <jakubkozik120@gmail.com>
2022-08-13 11:11:58 +0200

    Add env

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   session({
-    secret: 'xyz567',
+    secret: process.env.SESSION_SECRET,
     store: MongoStore.create(mongoose.connection),
   })
 );

b1745ab018fa13bdb00590a134ece834b869fd21
Jakub Kozik <jakubkozik120@gmail.com>
2022-08-24 16:53:25 +0200

    add heroku-postbuild script in package.json

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.use(
 
 app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/uploads/')));
 
 // add routes
 app.use('/api', require('./routes/ads.routes'));
